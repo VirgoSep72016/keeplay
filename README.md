6fb5296a38c2addeec142461ad189e0d31868431
peter <peter@Applede-iMac-3.local>
2016-09-08 15:44:06 +0800

    Add express server

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Keeplay
 The SEP72016 is a magic feature which makes your dream come true.
+
+1. `npm start` to build static file
+1. `npm run server` to run server at localhost:8080
---
+++ entry.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port = 8080
+
+app.use(express.static('build'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
