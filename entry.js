ebfdcca9ec624dee9b1fbbed7428d3a05823569d
peter <peter@Applede-iMac-3.local>
2016-09-08 17:16:27 +0800

    Refactory server

--- entry.js
+++ entry.js
@@ -1,14 +1,2 @@
-const express = require('express')
-const app = express()
-
-const port = 8080
-
-app.use(express.static('build'))
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + 'index.html');
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-});
+const server = require('./server')
+server.run();
